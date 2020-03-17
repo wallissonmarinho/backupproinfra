bb963047c121b21651bea9e6c8443a740ee96e66
Wallisson Marinho <wallissonmarinho@hotmail.com>
2020-03-17 20:08:12 -0300

    subindo projeto

---
+++ src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+function Login() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
+
+export default Login;
